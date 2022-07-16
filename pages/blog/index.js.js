1c0ab58f7d87c1702594f3a7bf0d844c2b8c8d90
MistbornL <lashaloria3@gmail.com>
2022-07-16 17:25:31 +0400

    nested routes

---
+++ pages/blog/index.js.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const blog = () => {
+  return <h1>Blog</h1>;
+};
---
+++ pages/blog/second.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function second() {
+  return <h1>second</h1>;
+}
