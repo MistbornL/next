b40b84a1e0d3bc2d56d06bcceaecc2b67142b3a7
MistbornL <lashaloria3@gmail.com>
2022-07-16 17:33:11 +0400

    dynamic routes

---
+++ pages/product/[productId].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const ProductDetail = () => {
+  const router = useRouter();
+  const productId = router.query.productId;
+  return <h1>Detail About Product {productId}</h1>;
+};
+
+export default ProductDetail;
