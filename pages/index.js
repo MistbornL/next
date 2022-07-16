e6f3cdff65ffbf9ff4d61fc3bf0018af0cb066d8
MistbornL <lashaloria3@gmail.com>
2022-07-16 19:06:06 +0400

    navigate programaticly

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import React from "react";
 
 const Home = () => {
+  const router = useRouter();
   return (
     <div>
       <h1>Home Page</h1>
-      <Link href={"/blog"}>
-        <a>Blog</a>
-      </Link>
+
       <Link href={"/product"}>
         <a>product</a>
       </Link>
+      <h2>
+        to view Blog Click on button{" "}
+        <button
+          onClick={() => {
+            router.push("/blog");
+          }}
+        >
+          click here
+        </button>
+      </h2>
     </div>
   );
 };
