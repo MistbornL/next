2190a0ceb2d4addb54e195822c6108f44bb263eb
MistbornL <lashaloria3@gmail.com>
2022-07-17 20:22:15 +0400

    dynami fetch

---
+++ pages/posts/[postId].js
@@ -0,0 +1,32 @@
+function Post({ post }) {
+  return (
+    <div>
+      <h2>
+        {post.id} {post.title}
+      </h2>
+    </div>
+  );
+}
+
+export default Post;
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
