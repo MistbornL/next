f01aa11e117a793be3c7f840e769dd7464fcd48f
MistbornL <lashaloria3@gmail.com>
2022-07-21 15:31:32 +0400

    api post request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -3,14 +3,35 @@ import { useState } from "react";
 
 export default function Comments() {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
     setComments(data);
     console.log(comments);
   };
+
+  const submitComment = async () => {
+    const response = await fetch("/api/comments", {
+      method: "Post",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/Json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div>
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => {
+          setComment(e.target.value);
+        }}
+      />
+      <button onClick={submitComment}>Submit Comment</button>
       <button onClick={fetchComments}>Load Comments</button>
       {comments.map((comment) => {
         return (
